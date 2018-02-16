1fd7a518b7c44e7f9da267a2f1409745056a2b16
fagasparelo <gaspawn@gmail.com>
2018-02-16 13:15:43 -0200

    inicial

---
+++ src/main/java/br/mp/mpf/twodb/ConfigDstDB.java
@@ -0,0 +1,41 @@
+package br.mp.mpf.twodb;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "dstEntityManagerFactory", transactionManagerRef = "dstTransactionManager", basePackages = "br.mp.mp.twoDB.dstDB.repo")
+public class ConfigDstDB {
+
+	@Bean(name = "dstDataSource")
+	@ConfigurationProperties(prefix = "destino.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = "dstEntityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean dstEntityManagerFactory(EntityManagerFactoryBuilder builder,
+			@Qualifier("dstDataSource") DataSource dataSource) {		
+		return builder.dataSource(dataSource).packages("br.mp.mp.twoDB.dstDB.domain").persistenceUnit("dstDB").build();
+	}
+
+	@Bean(name = "dstTransactionManager")
+	public PlatformTransactionManager dstTransactionManager(
+			@Qualifier("dstEntityManagerFactory") EntityManagerFactory barEntityManagerFactory) {
+		return new JpaTransactionManager(barEntityManagerFactory);
+	}
+
+}
---
+++ src/main/java/br/mp/mpf/twodb/TwodbApplication.java
@@ -0,0 +1,19 @@
+package br.mp.mpf.twodb;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodbApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwodbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
